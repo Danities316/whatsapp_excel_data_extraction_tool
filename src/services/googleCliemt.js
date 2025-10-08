5a3fe0592780c96c7615a7a0a023a7310a519ee1
Danities316 <attayi_316@hotmail.com>
2025-10-08 17:21:18 +0100

    refactor GOOGLE_SHEETS_CREDENTIALS

--- src/services/googleCliemt.js
+++ src/services/googleCliemt.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 
-const credentials = JSON.parse(fs.readFileSync(path.join(__dirname, '../../googlesheetAPI.json')));
+// const credentials = JSON.parse(fs.readFileSync(path.join(__dirname, '../../googlesheetAPI.json')));
+const credentials = JSON.parse(process.env.GOOGLE_SHEETS_CREDENTIALS);
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
 const auth = new google.auth.GoogleAuth({
